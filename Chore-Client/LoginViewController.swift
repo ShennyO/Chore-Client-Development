780ce1a62c3d1c474d3f533169be3c2ebcf2234b
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-02-07 13:44:50 -0800

    Linked outlets to LoginVC

---
+++ Chore-Client/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Chore-Client
+//
+//  Created by Sunny Ouyang on 2/7/18.
+//  Copyright © 2018 Sunny Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    
+
+}
