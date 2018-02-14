73349e76991e6be4caf01f69344c55a0b700a0cf
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-02-13 22:09:14 -0800

    started User Chores page

---
+++ Chore-Client/Cells/UserChoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserChoreTableViewCell.swift
+//  Chore-Client
+//
+//  Created by Sunny Ouyang on 2/13/18.
+//  Copyright © 2018 Sunny Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class UserChoreTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var choreNameLabel: UILabel!
+    @IBOutlet weak var chorePenaltyLabel: UILabel!
+    @IBOutlet weak var choreDescriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
---
+++ Chore-Client/ViewControllers/UserChoresViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserChoresViewController.swift
+//  Chore-Client
+//
+//  Created by Sunny Ouyang on 2/13/18.
+//  Copyright © 2018 Sunny Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class UserChoresViewController: UIViewController {
+
+    @IBOutlet weak var choresTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //in here we want to perform the GET requests for all of the current user's chores
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
